ae50abdd76e234c3b30da4ad24976df97734bfe5
Kiruthick <kiruthicksanboopathi@gmail.com>
2025-06-19 12:05:37 +0530

    learned how to create arrays

---
+++ array.js
@@ -0,0 +1,11 @@
+const mixedArray = [5 , "kiruthick" , [] ]
+
+
+const skills = ["Coding" , "Writing" , "Drawing"]
+
+//  Array constructor method
+
+const  genre =  new Array("Action" , "Adventure","Horror","Myestery","superNatural")
+
+console.log(genre)
+
